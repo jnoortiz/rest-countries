de444aca1a3a140ee3838235f4a6c5fc306b27fa
Joaquin Ortiz <jno.ortiz@gmail.com>
2021-04-06 08:00:13 +0800

    Cleanup spec files

--- src/app/shared/pipes/name-obj-join.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NameObjJoinPipe } from './name-obj-join.pipe';
-
-describe('NameObjJoinPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NameObjJoinPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
